26847fd9aba055d391c2984ebc87b75d710a4017
Your Name <you@example.com>
2023-03-08 13:45:06 +0500

    Signin,signup UI completed

---
+++ Frontend/Application/lib/screen/home/components/body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+
+import 'home_header.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: getProportionateScreenHeight(20)),
+            const HomeHeader(),
+            SizedBox(height: getProportionateScreenWidth(10)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Frontend/Application/lib/screen/splash/components/body.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:digital_scrum_assistant/constant.dart';
+import 'package:digital_scrum_assistant/screen/signin/signin_screen.dart';
+import 'package:digital_scrum_assistant/size_config.dart';
+
+// This is the best practice
+import '../components/splash_content.dart';
+import '../../../components/default_button.dart';
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to the digital scrumm assistant",
+      "image": "assets/images/splash_1.jpg"
+    },
+    {
+      "text": "Run Asynchronous Standup Meetings Instantly \n",
+      "image": "assets/images/splash_2.jpg"
+    },
+    {
+      "text": "Connect With Your Team. \nJust stay at home with us",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]['text'],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        splashData.length,
+                        (index) => buildDot(index: index),
+                      ),
+                    ),
+                    const Spacer(flex: 4),
+                    DefaultButton(
+                      text: "Continue",
+                      press: () {
+                        Navigator.pushNamed(context, SignInScreen.routeName);
+                      },
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int? index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: const EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : const Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}

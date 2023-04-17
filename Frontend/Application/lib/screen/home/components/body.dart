dbfe9c312d423eac1c971697568220d81a1c9e51
Your Name <you@example.com>
2023-04-17 16:18:50 +0500

    home screen update

--- Frontend/Application/lib/screen/home/components/body.dart
+++ Frontend/Application/lib/screen/home/components/body.dart
@@ -13,6 +13,14 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
+            // Logo/Branding
+            Image.asset(
+              'assets/images/logo.png', // Replace with your actual logo image path
+              width: getProportionateScreenWidth(
+                  100), // Set the desired width for your logo
+              height: getProportionateScreenWidth(
+                  100), // Set the desired height for your logo
+            ),
             SizedBox(height: getProportionateScreenHeight(20)),
             Padding(
               padding: EdgeInsets.symmetric(
@@ -20,41 +28,141 @@ class Body extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'Digital Scrum Assistant',
-                    style: TextStyle(
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: getProportionateScreenWidth(18),
+                  // Navigation/Menu
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          // Handle menu item 1 press
+                        },
+                        child: Text(
+                          'Menu Item 1',
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: getProportionateScreenWidth(14),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Handle menu item 2 press
+                        },
+                        child: Text(
+                          'Menu Item 2',
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: getProportionateScreenWidth(14),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Handle menu item 3 press
+                        },
+                        child: Text(
+                          'Menu Item 3',
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: getProportionateScreenWidth(14),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // CTA (Call-to-Action) Button
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle CTA button press
+                    },
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: getProportionateScreenWidth(14),
+                      ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(20)),
+            // Hero Section
+            Container(
+              height: getProportionateScreenHeight(
+                  200), // Set the desired height for your Hero Section
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/hero_image.png'), // Replace with your actual hero image path
+                  fit: BoxFit.cover,
+                ),
+              ),
+              // Add any additional widgets or content for your Hero Section
+            ),
+            SizedBox(height: getProportionateScreenHeight(20)),
+            // Feature Highlights
+            // Add your feature highlights widgets or content here
+            SizedBox(height: getProportionateScreenHeight(20)),
+            // Testimonials
+            // Add your testimonials widgets or content here
+            SizedBox(height: getProportionateScreenHeight(20)),
+            // Social Proof
+            // Add your social proof widgets or content here
+            SizedBox(height: getProportionateScreenHeight(20)),
+            // your footer widgets or content here
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(20)),
+              child: Column(
+                children: [
+                  // Add your footer content here, such as contact information, links, etc.
+                  Text(
+                    'Contact us: contact@example.com',
+                    style: TextStyle(
+                      color: kTextColor,
+                      fontSize: getProportionateScreenWidth(14),
+                    ),
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(10)),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       IconButton(
                         onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => FeaturePlayListScreen()),
-                          );
+                          // Handle social media icon 1 press
                         },
-                        icon: const Icon(Icons.featured_play_list),
+                        icon: const Icon(
+                          Icons.facebook,
+                          color: kPrimaryColor,
+                        ),
                       ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.person),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.info_outline),
-                      ),
+                      // IconButton(
+                      //   onPressed: () {
+                      //     // Handle social media icon 2 press
+                      //   },
+                      //   icon: Icon(
+                      //     Icons.twitter,
+                      //     color: kPrimaryColor,
+                      //   ),
+                      // ),
+                      // IconButton(
+                      //   onPressed: () {
+                      //     // Handle social media icon 3 press
+                      //   },
+                      //   icon: Icon(
+                      //     Icons.instagram,
+                      //     color: kPrimaryColor,
+                      //   ),
+                      // ),
                     ],
                   ),
                 ],
               ),
             ),
-            const HomeHeader(),
-            SizedBox(height: getProportionateScreenWidth(10)),
           ],
         ),
       ),
